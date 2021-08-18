b6de1a77b3c05bc1f62dcccf5937dc13b1532a9f
fe-alves <85088716+fe-alves@users.noreply.github.com>
2021-08-18 20:03:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// console.log('Hello world!');
+
+// prompt("Digite seu nome");
+
+/* 
+1
+let a = prompt('Qual seu nome?');
+let b = prompt('Qual sua turma?')
+
+console.log('Olá, '+ a +', seja bem vindo à turma '+ b);
+
+2
+let varTexto = "Isso é uma String";
+let varNumero = 50;
+let varBoolean = 1 > 2;
+
+console.log(varTexto);
+console.log(varNumero);
+console.log(varBoolean);
+
+typeof varBoolean
+
+3
+let arrayNome = ["Felipe", "Ana", "Gabriel", "Alice"];
+let arrayNumero = [16, 40, 20, 18];
+let arrayMisto = ["Juan", false, 27];
+
+console.log(arrayNome);
+console.log(arrayNumero);
+console.log(arrayMisto);
+
+4
+var a = "Turma C7";
+var b = a;
+
+console.log(b);
+*/
